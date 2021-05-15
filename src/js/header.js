9f4e702f113d76bb5681cf850c99f778700c4393
ThibertChristophe <c.thibert89@gmail.com>
2021-05-15 20:34:25 +0200

    menu hide and js

--- src/js/header.js
+++ src/js/header.js
@@ -2,7 +2,30 @@
 const country  = document.getElementById('country');
 const sousMenu  = document.getElementById('menuCache');
 
+const menuAbout  = document.querySelector('.menuAbout');
+const menuCacheAbout  = document.querySelector('.menuCache_about');
+
+
 country.addEventListener('click', () => {
+    removeAll('country');
     country.classList.toggle('active');
     sousMenu.classList.toggle('show');
-})
+});
+
+menuAbout.addEventListener('click', () => {
+    removeAll('about');
+    menuAbout.classList.toggle('active');
+    menuCacheAbout.classList.toggle('show');
+});
+
+const removeAll = (except) => {
+
+    if(except==='about'){
+        country.classList.remove('active');
+        sousMenu.classList.remove('show');
+    }
+    if(except==='country'){
+        menuAbout.classList.remove('active');
+        menuCacheAbout.classList.remove('show');
+    }
+}
