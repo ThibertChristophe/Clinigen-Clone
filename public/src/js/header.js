3241171e339b0d0e9972e533ee1a223bae3a3a7e
ThibertChristophe <c.thibert89@gmail.com>
2021-05-17 16:57:42 +0200

    index finis
    Reste la recherche et peaufinage peut-être

--- public/src/js/header.js
+++ public/src/js/header.js
@@ -7,6 +7,20 @@ const menuCacheAbout  = document.querySelector('.menuCache_about');
 const menuGlobalOffice = document.querySelector('.menuGlobaOffice');
 const menuCacheGlobalOffice = document.querySelector('.menuCache_globalOffice');
 
+const menuCareers = document.querySelector('.menuCareers');
+const menuCacheCareers = document.querySelector('.menuCache_careers');
+
+const menuNewsroom = document.querySelector('.menuNewsroom');
+const menuCacheNewsroom = document.querySelector('.menuCache_newsroom');
+
+const menuInvestors = document.querySelector('.menuInvestors');
+const menuCacheInvestors = document.querySelector('.menuCache_investors');
+
+const menuSustain = document.querySelector('.menuSustain');
+const menuCacheSustain = document.querySelector('.menuCache_sustain');
+
+
+
 const arrayOfLeftMenu = document.querySelectorAll('.header_top_menuLeft > nav > ul > li > a');
 
 menuCountry.addEventListener('click', (e) => {
@@ -28,10 +42,42 @@ menuAbout.addEventListener('click', (e) => {
 
 menuGlobalOffice.addEventListener('click', (e) => {
     removeAll(menuGlobalOffice.classList[0], true);
-   if (!e.target.classList.contains('actif')){
+    if (!e.target.classList.contains('actif')){
          menuCacheGlobalOffice.classList.add('show');
     }
     menuGlobalOffice.classList.toggle('actif');
+});
+
+menuCareers.addEventListener('click', (e) => {
+    removeAll(menuCareers.classList[0], true);
+    if (!e.target.classList.contains('actif')){
+        menuCacheCareers.classList.add('show');
+    }
+    menuCareers.classList.toggle('actif');
+});
+
+menuNewsroom.addEventListener('click', (e) => {
+    removeAll(menuNewsroom.classList[0], true);
+    if (!e.target.classList.contains('actif')){
+        menuCacheNewsroom.classList.add('show');
+    }
+    menuNewsroom.classList.toggle('actif');
+});
+
+menuInvestors.addEventListener('click', (e) => {
+    removeAll(menuInvestors.classList[0], true);
+    if (!e.target.classList.contains('actif')){
+        menuCacheInvestors.classList.add('show');
+    }
+    menuInvestors.classList.toggle('actif');
+});
+
+menuSustain.addEventListener('click', (e) => {
+    removeAll(menuSustain.classList[0], true);
+    if (!e.target.classList.contains('actif')){
+        menuCacheSustain.classList.add('show');
+    }
+    menuSustain.classList.toggle('actif');
 });
 
 /**
@@ -46,6 +92,11 @@ const removeAll = (except, country) => {
     menuCacheCountry.classList.remove('show');
     menuCacheAbout.classList.remove('show');
     menuCacheGlobalOffice.classList.remove('show');
+    menuCacheCareers.classList.remove('show');
+    menuCacheNewsroom.classList.remove('show');
+    menuCacheInvestors.classList.remove('show');
+    menuCacheSustain.classList.remove('show');
+
 
     arrayOfLeftMenu.forEach(e => {
         if (e.classList[0] !== except){
