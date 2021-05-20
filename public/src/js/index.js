3a1ba06eb38b6f5f74386fe0a2b2f5fd8895d6c5
ThibertChristophe <c.thibert89@gmail.com>
2021-05-20 17:00:16 +0200

    fini page mission
    + réorga des fichiers css et dossier

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,47 +1,42 @@
+const sousSlideL = document.getElementById("sousSlider_L");
+const scgL = document.querySelectorAll(".svg1");
+const sousSlideM = document.getElementById("sousSlider_M");
+const scgM = document.querySelectorAll(".svg2");
+const sousSlideR = document.getElementById("sousSlider_R");
+const scgR = document.querySelectorAll(".svg3");
 
-const sousSlideL = document.getElementById('sousSlider_L');
-const scgL = document.querySelectorAll('.svg1');
-const sousSlideM = document.getElementById('sousSlider_M');
-const scgM = document.querySelectorAll('.svg2');
-const sousSlideR = document.getElementById('sousSlider_R');
-const scgR = document.querySelectorAll('.svg3');
+sousSlideL.addEventListener("mouseover", () => {
+  scgL.forEach((element) => {
+    element.style.fill = "#005052";
+  });
+});
 
+sousSlideL.addEventListener("mouseout", () => {
+  scgL.forEach((element) => {
+    element.style.fill = "#8cdb1f";
+  });
+});
 
-sousSlideL.addEventListener('mouseover', () => {
-    scgL.forEach(element => {
-        element.style.fill='#005052';
-    });
-})
+sousSlideM.addEventListener("mouseover", () => {
+  scgM.forEach((element) => {
+    element.style.fill = "#005052";
+  });
+});
 
-sousSlideL.addEventListener('mouseout', () => {
-    scgL.forEach(element => {
-        element.style.fill='#8cdb1f';
-    });
-})
+sousSlideM.addEventListener("mouseout", () => {
+  scgM.forEach((element) => {
+    element.style.fill = "#8cdb1f";
+  });
+});
 
-sousSlideM.addEventListener('mouseover', () => {
-    scgM.forEach(element => {
-        element.style.fill='#005052';
-       
-    });
-})
+sousSlideR.addEventListener("mouseover", () => {
+  scgR.forEach((element) => {
+    element.style.fill = "#005052";
+  });
+});
 
-sousSlideM.addEventListener('mouseout', () => {
-    scgM.forEach(element => {
-        element.style.fill='#8cdb1f';
-    });
-})
-
-sousSlideR.addEventListener('mouseover', () => {
-    scgR.forEach(element => {
-        element.style.fill='#005052';
-       
-    });
-})
-
-sousSlideR.addEventListener('mouseout', () => {
-    scgR.forEach(element => {
-        element.style.fill='#8cdb1f';
-    });
-})
-
+sousSlideR.addEventListener("mouseout", () => {
+  scgR.forEach((element) => {
+    element.style.fill = "#8cdb1f";
+  });
+});
---
+++ public/src/js/mission.js
@@ -0,0 +1,68 @@
+const mainHeader_containt = document.querySelector(".mainHeader_containt");
+const menuFixed = document.querySelector(".menuFixed");
+
+const lien = document.querySelectorAll(".menuFixed > ul > li > a");
+
+const rightMedicine = document.querySelector(".rightMedicine");
+const rightPatient = document.querySelector(".rightPatient");
+const rightTime = document.querySelector(".rightTime");
+
+let menuFixed_coord;
+let mainHeader_containt_coord;
+let rightMedicine_coord;
+let rightPatient_coord;
+let rightTime_coord;
+
+let fixed = false;
+document.addEventListener("scroll", () => {
+  menuFixed_coord = menuFixed.getBoundingClientRect();
+  mainHeader_containt_coord = mainHeader_containt.getBoundingClientRect();
+
+  /** Methode pour detacher le menu et le rendre fixe et le faire suivre le scroll puis le remettre en place en revenant en haut de page */
+  if (menuFixed_coord.top <= 140) {
+    if (fixed === false) {
+      menuFixed.style.background = "rgba(0,27,43,0.9)";
+      menuFixed.style.position = "fixed";
+      menuFixed.style.top = "140px";
+      lien.item(0).style.color = "#8cdb1f";
+      fixed = true;
+    }
+  }
+  if (fixed) {
+    if (mainHeader_containt_coord.top >= -261) {
+      menuFixed.style.background = "transparent";
+      menuFixed.style.position = "relative";
+      menuFixed.style.top = "0";
+      lien.item(0).style.color = "rgb(185, 185, 185)";
+      fixed = false;
+    }
+  }
+
+  rightMedicine_coord = mainHeader_containt.getBoundingClientRect();
+  rightPatient_coord = mainHeader_containt.getBoundingClientRect();
+  rightTime_coord = mainHeader_containt.getBoundingClientRect();
+  /** Methode pour allumer les liens en fonctions du scroll */
+  if (rightMedicine_coord.top <= -1031) {
+    lien.item(1).style.color = "#8cdb1f";
+    lien.item(0).style.color = "rgb(185, 185, 185)";
+  } else {
+    if (fixed === true) {
+      lien.item(0).style.color = "#8cdb1f";
+    }
+    lien.item(1).style.color = "rgb(185, 185, 185)";
+  }
+  if (rightPatient_coord.top <= -1804) {
+    lien.item(2).style.color = "#8cdb1f";
+    lien.item(1).style.color = "rgb(185, 185, 185)";
+  } else {
+    lien.item(2).style.color = "rgb(185, 185, 185)";
+  }
+  console.log(rightTime_coord.top);
+  if (rightTime_coord.top <= -2648) {
+    lien.item(3).style.color = "#8cdb1f";
+    lien.item(1).style.color = "rgb(185, 185, 185)";
+    lien.item(2).style.color = "rgb(185, 185, 185)";
+  } else {
+    lien.item(3).style.color = "rgb(185, 185, 185)";
+  }
+});
