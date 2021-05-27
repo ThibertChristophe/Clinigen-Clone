661b366aca051c76108c3658921103f7316c6df7
ThibertChristophe <c.thibert89@gmail.com>
2021-05-27 17:09:57 +0200

    menu and js in pharma page

---
+++ public/src/js/pharma.js
@@ -0,0 +1,6 @@
+const combo1 = document.querySelector(".combo1");
+const sousMenu1 = document.querySelector(".sousMenu1");
+
+combo1.addEventListener("click", () => {
+  sousMenu1.classList.toggle("show");
+});
