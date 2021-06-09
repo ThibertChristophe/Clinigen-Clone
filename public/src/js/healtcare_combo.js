d4c553b185637b46bc390a6fc297cc98c6a0adeb
ThibertChristophe <c.thibert89@gmail.com>
2021-06-09 16:02:24 +0200

    End all page in menu
    Fini toutes les pages du menu principal
    TODO : formulaire sur chaque page adapté sous menu des combo

--- public/src/js/healtcare_combo.js
+++ public/src/js/healtcare_combo.js
@@ -8,6 +8,14 @@ const formulaire_sousMenu1 = document.querySelector(".formulaire_sousMenu1");
 const formulaire_sousMenu2 = document.querySelector(".formulaire_sousMenu2");
 const formulaire_sousMenu3 = document.querySelector(".formulaire_sousMenu3");
 
+// permet de placer les sous menu en dessous de chaque combo
+formulaire_sousMenu1.style.top =
+  formulaire_combo1.getBoundingClientRect().top + 52 + window.scrollY + "px";
+formulaire_sousMenu2.style.top =
+  formulaire_combo2.getBoundingClientRect().top + 52 + window.scrollY + "px";
+formulaire_sousMenu3.style.top =
+  formulaire_combo3.getBoundingClientRect().top + 52 + window.scrollY + "px";
+
 formulaire_combo1.addEventListener("click", () => {
   formulaire_sousMenu1.classList.toggle("show");
   formulaire_sousMenu2.classList.remove("show");
@@ -30,7 +38,6 @@ const formulaire_option1 = document.querySelectorAll(".formulaire_option1");
 
 formulaire_option1.forEach((e) => {
   e.addEventListener("click", () => {
-    console.log(e.innerHTML);
     formulaire_combo1.innerHTML = e.innerHTML;
     formulaire_sousMenu1.classList.toggle("show");
   });
@@ -40,7 +47,6 @@ const formulaire_option2 = document.querySelectorAll(".formulaire_option2");
 
 formulaire_option2.forEach((e) => {
   e.addEventListener("click", () => {
-    console.log(e.innerHTML);
     formulaire_combo2.innerHTML = e.innerHTML;
     formulaire_sousMenu2.classList.toggle("show");
   });
@@ -50,7 +56,6 @@ const formulaire_option3 = document.querySelectorAll(".formulaire_option3");
 
 formulaire_option3.forEach((e) => {
   e.addEventListener("click", () => {
-    console.log(e.innerHTML);
     formulaire_combo3.innerHTML = e.innerHTML;
     formulaire_sousMenu3.classList.toggle("show");
   });
